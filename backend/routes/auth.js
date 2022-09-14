6fecfb2901bb44587f55f3f751eff7924b599a3e
netrahindocha <netrahindocha1@gmail.com>
2022-09-14 17:44:57 +0530

    Hashing passwords using bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Justchill$dude';
 
 // Create a User using: POST "/api/auth". No login required
 router.post('/createuser', [
@@ -14,19 +18,30 @@ router.post('/createuser', [
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    // Check whether the user with this email exists already
     try{
+      // Check whether the user with this email exists already
     let user = await User.findOne({email: req.body.email});
     if(user){
       return res.status(400).json({error: "Sorry a user with this email already exists"})
     }
+    const salt = await bcrypt.genSalt(10);
+    secPass = await bcrypt.hash(req.body.password, salt)
     // Create a new user
     user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
       }) 
-      res.json(user);
+
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      const authtoken = jwt.sign(data, JWT_SECRET);
+
+      // res.json(user);
+      res.json({authtoken})
       
     } catch(error) {
       console.error(error.message);
