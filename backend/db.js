c218b64cec1eb7ec91573fa2214b2ef3c8f1d0e1
netrahindocha <netrahindocha1@gmail.com>
2022-09-15 15:22:49 +0530

    Formatting doc & fixing bugs

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
-
 const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
-
-const connectToMongo = async() => {
+const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongo successfully");
+        console.log("Connected to Mongo Successfully");
     })
 }
 
--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
+
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
