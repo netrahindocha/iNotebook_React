b7d068ad49f424dc34599665eae9216bf7e34f7b
netrahindocha <netrahindocha1@gmail.com>
2022-09-16 03:04:47 +0530

    Fetching Notes from Context

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/noteContext'
 
 const Home = () => {
+  const {notes, setNotes} = useContext(noteContext);
   return (
     <div>
       <div className="container my-3">
@@ -25,6 +27,9 @@ const Home = () => {
 
       <div className="container my-3">
         <h2>Your Notes</h2>
+        {notes.map((note) => {
+          return note.title;
+        })}
       </div>
     </div>
   )
