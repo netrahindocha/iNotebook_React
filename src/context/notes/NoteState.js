38c1858447d19a3a30c68a1a3828c2234e381110
netrahindocha <netrahindocha1@gmail.com>
2022-09-16 16:48:17 +0530

    Added AddNote component

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -69,9 +69,34 @@ const NoteState = (props) => {
     ]
     const [notes, setNotes] = useState(notesInit)
 
+    // Add a Note
+    const addNote = (title, description, tag) => {
+        // TODO: API Call
+        console.log("Adding a new note")
+        let note = {
+            "_id": "61322f19553781a8ca8d0e085",
+            "user": "6131dc5e3e4037cd4734a066",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "date": "2021-09-03T14:20:09.668Z",
+            "__v": 0
+        }
+        setNotes(notes.concat(note));
+    }
+
+    // Delete a Note
+    const deleteNote = () => {
+
+    }
+
+    // Edit a Note
+    const editNote = () => {
+        
+    }
 
     return (
-        <noteContext.Provider value={{notes, setNotes}}>
+        <noteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
             {props.children}
         </noteContext.Provider>
     )
