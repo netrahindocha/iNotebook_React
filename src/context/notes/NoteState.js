b570ca34a46c259a0fb19538b57bfa0b2032ba87
netrahindocha <netrahindocha1@gmail.com>
2022-09-16 16:02:55 +0530

    Added icons && fixed bugs && created alert component

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -22,7 +22,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "61322f19553781a8ca8d0e08",
+            "_id": "61322f19553781a8ca8d0e081",
             "user": "6131dc5e3e4037cd4734a066",
             "title": "My Title",
             "description": "Please wake up early",
@@ -31,7 +31,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "61322f19553781a8ca8d0e08",
+            "_id": "61322f19553781a8ca8d0e082",
             "user": "6131dc5e3e4037cd4734a066",
             "title": "My Title",
             "description": "Please wake up early",
@@ -40,7 +40,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "61322f19553781a8ca8d0e08",
+            "_id": "61322f19553781a8ca8d0e083",
             "user": "6131dc5e3e4037cd4734a066",
             "title": "My Title",
             "description": "Please wake up early",
@@ -49,7 +49,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "61322f19553781a8ca8d0e08",
+            "_id": "61322f19553781a8ca8d0e084",
             "user": "6131dc5e3e4037cd4734a066",
             "title": "My Title",
             "description": "Please wake up early",
@@ -58,7 +58,7 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
-            "_id": "61322f19553781a8ca8d0e08",
+            "_id": "61322f19553781a8ca8d0e085",
             "user": "6131dc5e3e4037cd4734a066",
             "title": "My Title",
             "description": "Please wake up early",
