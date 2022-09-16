e68bb996a5fdfceb0baac8188bb5faad1f8874c9
netrahindocha <netrahindocha1@gmail.com>
2022-09-17 01:01:58 +0530

    Added delete functionality

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import noteContext from "../context/notes/noteContext"
 
 const NoteItem = (props) => {
-    const { note } = props;
+    const context = useContext(noteContext);
+    const { deleteNote } = context;
+    const {note} = props;
     return (
         <div className='col-md-3'>
             <div className="card my-3">
                 <div className="card-body">
                     <div className="d-flex align-items-center">
                     <h5 className="card-title">{note.title}</h5>
-                    <i className="fa-regular fa-trash-can mx-2" id="delete"></i>
+                    <i className="fa-regular fa-trash-can mx-2" id="delete" onClick={() => {deleteNote(note._id)}}></i>
                     <i className="fa-regular fa-pen-to-square mx-2" id="edit"></i>
                     </div>
                     <p className="card-text">{note.description}</p>
